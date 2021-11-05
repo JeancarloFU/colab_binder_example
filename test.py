11855af2461cc4511c949721da5e0237ba003808
Jeancarlo Fajardo Urbina <jfajardou@gmail.com>
2021-11-05 02:45:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 #--------------------------
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import xarray as xr
 
 print("--------------")
 
-
 #--------------------------
-a=np.arange(5)
+a=np.arange(10)
 plt.plot(a,a,'b')
 plt.text('text1')
 
