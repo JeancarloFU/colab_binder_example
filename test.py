d02723b4247b740e652eba45691d8c6f2f3ac194
JeancarloFU <jfajardou@gmail.com>
2021-11-05 02:48:07 +0100

    new4

--- test.py
+++ test.py
@@ -2,13 +2,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
+import torch
+import pandas as pd
 
 print("--------------")
 
 
 #--------------------------
 a=np.arange(5)
-plt.plot(a,a,'b')
+plt.plot(a,a,'b',lw=1)
+plt.title('abc')
 plt.text('text1')
 
 
